a2714374284edba637a6abb4444653bb10076642
J. Matthew Pryor <jmp@tenacious.ventures>
2021-11-22 14:40:52 +1100

    code review feedback, remove console logs, v1.0.6

--- src/components/heatmap/index.tsx
+++ src/components/heatmap/index.tsx
@@ -20,49 +20,6 @@ export interface HeatMapSettings {
   }
 }
 
-addStyle(`
-  svg.roamjs-heatmap {
-    color: #24292e;
-    -webkit-user-select: none;
-            user-select: none;
-    display: block;
-    font-size: 10px;
-  }
-  svg.roamjs-heatmap rect {
-    display: block;
-    cursor: pointer;
-  }
-  svg.roamjs-heatmap rect:hover {
-    stroke: rgba(0, 0, 0, 0.14);
-    stroke-width: 1px;
-  }
-  svg.roamjs-heatmap rect:active {
-    fill: #196127;
-    stroke-width: 0;
-  }
-  svg.roamjs-heatmap text {
-    text-anchor: middle;
-    font-size: inherit;
-    fill: currentColor;
-  }
-  .roamjs-heatmap .legend-0 {
-    fill: #EBEDF0;
-  }
-  .roamjs-heatmap .legend-4 {
-    fill: #C6E48B;
-  }
-  .roamjs-heatmap .legend-8  {
-    fill: #7BC96F;
-  }
-  .roamjs-heatmap .legend-12 {
-    fill: #239A3B;
-  }
-  .roamjs-heatmap .legend-32 {
-    fill: #196127;
-  }
-
-`);
-
 export * from './SVG';
 
 export interface HeatMapProps extends SVGProps {
--- src/index.ts
+++ src/index.ts
@@ -64,6 +64,48 @@ function parseConfig( tree: TreeNode): Options | string[] | string | number[] | 
   }
 }
 
+addStyle(`
+  svg.roamjs-heatmap {
+    color: #24292e;
+    -webkit-user-select: none;
+            user-select: none;
+    display: block;
+    font-size: 10px;
+  }
+  svg.roamjs-heatmap rect {
+    display: block;
+    cursor: pointer;
+  }
+  svg.roamjs-heatmap rect:hover {
+    stroke: rgba(0, 0, 0, 0.14);
+    stroke-width: 1px;
+  }
+  svg.roamjs-heatmap rect:active {
+    fill: #196127;
+    stroke-width: 0;
+  }
+  svg.roamjs-heatmap text {
+    text-anchor: middle;
+    font-size: inherit;
+    fill: currentColor;
+  }
+  .roamjs-heatmap .legend-0 {
+    fill: #EBEDF0;
+  }
+  .roamjs-heatmap .legend-4 {
+    fill: #C6E48B;
+  }
+  .roamjs-heatmap .legend-8  {
+    fill: #7BC96F;
+  }
+  .roamjs-heatmap .legend-12 {
+    fill: #239A3B;
+  }
+  .roamjs-heatmap .legend-32 {
+    fill: #196127;
+  }
+`);
+
 runExtension(ID, () => {
   createConfigObserver({
     title: CONFIG,
@@ -103,11 +145,9 @@ runExtension(ID, () => {
 
       let tree = getTreeByBlockUid(blockUid);
       let config: HeatMapSettings = parseConfig(tree) as HeatMapSettings;
-      console.log("ORIG CONFIG", config);
       config = merge.withOptions(
         { mergeArrays: false },
         defaultSettings, config);
-      console.log("MERGED CONFIG", config);
 
       if (config.range.startDate && config.range.endDate) {
         config.range.endDate = endOfDay(config.range.endDate);
@@ -131,8 +171,6 @@ runExtension(ID, () => {
       if (typeof config.query?.refs === "string") {
         config.query.refs = [config.query.refs];
       }
-
-      console.log(`FINAL CONFIG: `, config);
 
       renderHeatmap(
         config,
